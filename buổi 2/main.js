e2fce007e8685f469790b3abb0721014bbbf1e75
mirasi <minnmin1344@gmail.com>
2022-12-15 11:57:12 +0700

    BTVN 2

---
+++ buổi 2/main.js
@@ -0,0 +1,38 @@
+//var number = 1
+
+
+// while(number < 5){
+//     console.log(number)
+// }
+
+var food = [["grape", 15], ["apple", 16], ["mango", 20]]
+
+// food.push = [] 
+
+//console.log("I have ",food[0][1],food[0][0] )
+
+for (let i = 0; i < food.length; i++) {
+    console.log(food[i])
+}
+
+
+var mydog = {
+    "name": "ngao",
+    "leg": 4,
+    "friends": ["everything!"]
+}
+
+mydog.name = "Husky"
+mydog.color ="Brown"
+delete mydog.friends
+console.log(mydog)
+
+
+
+
+
+
+
+
+
+
